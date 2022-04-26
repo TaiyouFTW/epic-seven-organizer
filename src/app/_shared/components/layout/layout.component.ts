8d769ad7d7b5ab2c20038f2fbe9e6fc92b8c1988
Guilherme <gui.bm.1995@gmail.com>
2022-04-26 16:02:30 -0300

    responsive sidenav

--- src/app/_shared/components/layout/layout.component.ts
+++ src/app/_shared/components/layout/layout.component.ts
@@ -1,4 +1,6 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Observable, map, shareReplay } from 'rxjs';
 import { ConfigService } from '../../services/config.service';
 
 @Component({
@@ -10,8 +12,17 @@ export class LayoutComponent implements OnInit {
 
   colorTheme: string = 'light';
 
-  constructor(private renderer: Renderer2, private configService: ConfigService) {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+
+  constructor(private renderer: Renderer2, private configService: ConfigService, private breakpointObserver: BreakpointObserver) {
     this.changeTheme(this.configService.currentThemePreferenceValue);
+    console.log(this.isHandset$);
   }
 
   ngOnInit(): void {
