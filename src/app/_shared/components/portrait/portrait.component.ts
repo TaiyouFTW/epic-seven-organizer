b2affb417e16e753973a81821e9fb9e466ea0b7f
Guilherme <gui.bm.1995@gmail.com>
2022-05-10 02:12:41 -0300

    fix error when doesn't have hero code

--- src/app/_shared/components/portrait/portrait.component.ts
+++ src/app/_shared/components/portrait/portrait.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PortraitComponent implements OnInit {
 
   @Input() unknown?: string;
-  @Input() code?: string;
+  @Input() code?: string = 'npc0000';
   @Input() name?: string;
   @Input() role?: string;
   @Input() element?: string;
@@ -27,6 +27,7 @@ export class PortraitComponent implements OnInit {
   }
 
   onError(event: any) {
-    event.target.src = `https://www.e7vau.lt/static/game/face/${this.code}_s.png`;
+    let codeOnError = this.code == null || this.code == undefined || this.code == '' ? 'npc0000' : this.code;
+    event.target.src = `https://www.e7vau.lt/static/game/face/${codeOnError}_s.png`;
   }
 }
