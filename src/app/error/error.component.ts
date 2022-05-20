29f8a9d77eab546732f366912fd5515c474d3c16
Guilherme <gui.bm.1995@gmail.com>
2022-05-20 15:36:58 -0300

    #2kwm59n - Implement Error Page

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ConfigService } from '../_shared/services/config.service';
 
 @Component({
   selector: 'app-error',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  colorTheme: string = 'light';
+
+  constructor(private renderer: Renderer2, private configService: ConfigService) {
+    this.changeTheme(this.configService.currentThemePreferenceValue);
+  }
 
   ngOnInit(): void {
   }
 
+  changeTheme(theme: string) {
+    this.colorTheme = theme;
+    this.renderer.setAttribute(document.documentElement, 'data-theme', this.colorTheme);
+    this.configService.themePreferences(this.colorTheme);
+  }
 }
