1e5da94398ec02f26e0e40458abeb8107e5b79cc
Guilherme <gui.bm.1995@gmail.com>
2022-05-29 05:18:30 -0300

    test 5

--- api/heroes.ts
+++ api/heroes.ts
@@ -1,20 +1,18 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default (request: VercelRequest, response: VercelResponse) => {
-    response.setHeader('Access-Control-Allow-Credentials', 'true');
-    response.setHeader('Access-Control-Allow-Origin', '*');
-
-    let myHeaders = new Headers();
-    myHeaders.append('gc_currentPage', '0');
-    myHeaders.append('gc_hero', 'c4071');
-    myHeaders.append('gc_isPaging', 'N');
-    myHeaders.append('gc_lang', 'en');
-    myHeaders.append('gc_world', 'world_global');
-
-    let myInit = {
+export default async (request: VercelRequest, response: VercelResponse) => {
+    const res = await fetch('http://epic7.smilegatemegaport.com/guide/catalyst/getHeroFirstSet', {
         method: 'POST',
-        headers: myHeaders
-    };
-    const heroes = fetch('http://epic7.smilegatemegaport.com/guide/catalyst/getHeroFirstSet', myInit).then(response => response.json());
-    return response.status(200).send(heroes);
+        headers: {
+            'Content-Type': 'application/json',
+            'gc_currentPage': '0',
+            'gc_hero': 'c4071',
+            'gc_isPaging': 'N',
+            'gc_lang': 'en',
+            'gc_world': 'world_global'
+        },
+    });
+
+    const data = await res.json();
+    return response.status(200).json({ data });
 };
