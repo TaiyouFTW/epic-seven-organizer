ae29e60128ea4671302f8e35f093602d243e7ffd
Guilherme <gui.bm.1995@gmail.com>
2022-05-29 04:48:55 -0300

    cors

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (request: VercelRequest, response: VercelResponse) => {
     const { name } = request.query;
+    response.setHeader('Access-Control-Allow-Credentials', 'true');
+    response.setHeader('Access-Control-Allow-Origin', '*');
     response.status(200).send(`Hello ${name}!`);
 };
