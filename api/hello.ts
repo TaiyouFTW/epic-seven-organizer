5e8e70c668d48ef2a303c12352e7ad840283706c
Guilherme <gui.bm.1995@gmail.com>
2022-06-01 02:03:05 -0300

    test

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,14 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default (request: VercelRequest, response: VercelResponse) => {
+export default async (request: VercelRequest, response: VercelResponse) => {
     const { name } = request.query;
     response.setHeader('Access-Control-Allow-Credentials', 'true');
     response.setHeader('Access-Control-Allow-Origin', '*');
+
+    const res = await fetch('https://api.hgbrasil.com/weather??woeid=449648');
+
+    const data = await res.json();
+    return response.status(200).json(data);
+
     response.status(200).send(`Hello ${name}!`);
 };
