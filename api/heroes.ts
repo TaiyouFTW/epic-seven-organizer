9f042ddc96e47fa946edc97b35bdb620c7bb6e54
Guilherme <gui.bm.1995@gmail.com>
2022-06-01 01:52:54 -0300

    again

--- api/heroes.ts
+++ api/heroes.ts
@@ -8,13 +8,14 @@ export default async (request: VercelRequest, response: VercelResponse) => {
         "gc_isPaging": "N",
         "gc_lang": "en",
         "gc_world": "world_global",
-        "gc_hero": "c4071"
+        "gc_hero": "c4071",
     }
 
-    return response.send(fetch("http://epic7.smilegatemegaport.com/guide/catalyst/getHeroFirstSet", {
-        method: "POST",
-        headers: headersList
-    }).then(function (response) {
-        return response.json();
-    }));
+    const res = await fetch('http://epic7.smilegatemegaport.com/guide/catalyst/getHeroFirstSet', {
+        method: 'POST',
+        headers: headersList,
+    });
+
+    const data = await res.json();
+    return response.status(200).json(data);
 };
