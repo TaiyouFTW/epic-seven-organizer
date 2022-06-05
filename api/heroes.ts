2597b2381a6059660b71805cf37b129aa9603383
Guilherme <gui.bm.1995@gmail.com>
2022-06-05 00:47:17 -0300

    response.body

--- api/heroes.ts
+++ api/heroes.ts
@@ -22,18 +22,18 @@ export default async (req: VercelRequest, res: VercelResponse) => {
                 }
             }, (error, response, body) => {
                 if (response.statusCode == 200) {
-                    for (let i = 0; i < response.body.heroList.length; i++) {
-                        let hero = response.body.heroList[i];
-                        heroes.push({
-                            code: hero.heroCd,
-                            name: hero.heroNm,
-                            grade: hero.grade,
-                            jobCode: fixJobCode(hero.jobCd),
-                            attributeCode: fixAttributeCode(hero.attributeCd),
-                        });
+                    // for (let i = 0; i < response.body.heroList.length; i++) {
+                    //     let hero = response.body.heroList[i];
+                    //     heroes.push({
+                    //         code: hero.heroCd,
+                    //         name: hero.heroNm,
+                    //         grade: hero.grade,
+                    //         jobCode: fixJobCode(hero.jobCd),
+                    //         attributeCode: fixAttributeCode(hero.attributeCd),
+                    //     });
 
-                    }
-                    res.status(200).send(heroes);
+                    // }
+                    res.status(200).send(response.body);
                 } else {
                     res.status(response.statusCode).send(error.message);
                 }
