dffa46fe8c78bf445c30a13c1d357906a9027288
Guilherme <gui.bm.1995@gmail.com>
2022-06-05 00:49:55 -0300

    heroes

--- api/heroes.ts
+++ api/heroes.ts
@@ -1,12 +1,9 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import request from 'request';
-import { Hero } from './../src/app/_shared/interfaces/hero';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    let heroes = Array<Hero>();
 
     if (req.method === 'GET') {
         request(
@@ -22,17 +19,6 @@ export default async (req: VercelRequest, res: VercelResponse) => {
                 }
             }, (error, response, body) => {
                 if (response.statusCode == 200) {
-                    // for (let i = 0; i < response.body.heroList.length; i++) {
-                    //     let hero = response.body.heroList[i];
-                    //     heroes.push({
-                    //         code: hero.heroCd,
-                    //         name: hero.heroNm,
-                    //         grade: hero.grade,
-                    //         jobCode: fixJobCode(hero.jobCd),
-                    //         attributeCode: fixAttributeCode(hero.attributeCd),
-                    //     });
-
-                    // }
                     res.status(200).send(response.body);
                 } else {
                     res.status(response.statusCode).send(error.message);
@@ -41,23 +27,3 @@ export default async (req: VercelRequest, res: VercelResponse) => {
         );
     }
 };
-
-function fixJobCode(jobCode: string) {
-    switch (jobCode) {
-        case 'assassin':
-            return 'thief';
-        case 'manauser':
-            return 'soul-weaver';
-        default:
-            return jobCode;
-    }
-}
-
-function fixAttributeCode(attributeCode: string) {
-    switch (attributeCode) {
-        case 'wind':
-            return 'earth';
-        default:
-            return attributeCode;
-    }
-}
