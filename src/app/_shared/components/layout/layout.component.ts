77b74c6415d607316de236e8cb5c9ba34e576326
Guilherme <gui.bm.1995@gmail.com>
2022-11-22 15:50:57 -0300

    responsive rule removed

--- src/app/_shared/components/layout/layout.component.ts
+++ src/app/_shared/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -7,20 +7,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  colorTheme: string = 'light';
-
-  getScreenWidth: number;
-  sidenavOver: boolean = false;
-
-  @HostListener('window:resize', ['$event'])
-  onWindowResize() {
-    this.getScreenWidth = window.innerWidth;
-    this.sidenavOver = this.getScreenWidth < 1065;
-  }
-
   constructor() {
-    this.getScreenWidth = window.innerWidth;
-    this.sidenavOver = this.getScreenWidth < 1065;
   }
 
   ngOnInit(): void {
