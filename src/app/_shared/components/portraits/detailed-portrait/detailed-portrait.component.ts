81edabcaf68fe8b652b9333fa31cc40eb11bdd68
Guilherme <gui.bm.1995@gmail.com>
2023-02-26 04:32:18 -0300

    #864duz34v[DONE] - detailed portrait

---
+++ src/app/_shared/components/portraits/detailed-portrait/detailed-portrait.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Artifact } from 'src/app/_shared/interfaces/artifact';
+import { Hero } from 'src/app/_shared/interfaces/hero';
+import { PortraitService } from 'src/app/_shared/services/portrait.service';
+
+@Component({
+  selector: 'app-detailed-portrait',
+  templateUrl: './detailed-portrait.component.html',
+  styleUrls: ['./detailed-portrait.component.scss']
+})
+export class DetailedPortraitComponent implements OnInit {
+
+  source: string = '';
+  alt: string = '';
+
+  private _hero: Hero = {} as Hero;
+  @Input()
+  get hero(): Hero { return this._hero }
+  set hero(hero: Hero) {
+    this._hero = hero;
+    this.alt = hero.name;
+    this.source = this.portraitService.hero(hero.code);
+  }
+
+  @Input() mini?: boolean;
+
+  constructor(private portraitService: PortraitService) { }
+
+  ngOnInit(): void {
+  }
+
+  error(event: Event) {
+    const src = this.portraitService.error(this.hero);
+    let imageTarget = (event.target as HTMLImageElement);
+    imageTarget.src = src;
+  }
+
+}
