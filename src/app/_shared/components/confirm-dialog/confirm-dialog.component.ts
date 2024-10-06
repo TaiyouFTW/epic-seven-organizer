de3fde1d9a599f4bf4ccab7e909108e3d696a52f
Guilherme <gui.bm.1995@gmail.com>
2024-10-06 03:43:11 -0300

    Fixed import

--- src/app/_shared/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/_shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { DialogModule } from '@angular/cdk/dialog';
 import { ConfirmDialog } from '../../interfaces/confirm-dialog';
@@ -8,7 +8,7 @@ import { ConfirmDialog } from '../../interfaces/confirm-dialog';
 @Component({
   selector: 'app-confirm-dialog',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, DialogModule],
+  imports: [CommonModule, MatButtonModule, DialogModule, MatDialogModule],
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.scss']
 })
@@ -17,9 +17,6 @@ export class ConfirmDialogComponent {
     @Inject(MAT_DIALOG_DATA) public data: ConfirmDialog,
     private _dialogRef: MatDialogRef<ConfirmDialogComponent>,
   ) { }
-
-  ngOnInit(): void {
-  }
 
   confirm() {
     this._dialogRef.close(true);
